ef0df56910963f60eb95fbc6aa5819a4183bdd4f
Matheus Mendes <bigodines@gmail.com>
2012-10-03 21:23:07 -0300

    comparison and flagging revisions is working. ready to start with the screenshots management :)

--- severino.py
+++ severino.py
@@ -4,7 +4,7 @@ from app.storage import database
 
 class Severino(object): 
 
-    def __init__(self, base=None, current=None, db="storage/severino.db", rev=None):
+    def __init__(self, base=None, current=None, db="app/storage/severino.db", rev=None):
         self.base = base
         self.current = current
         self.rev = rev
@@ -20,15 +20,35 @@ class Severino(object):
             self.current = current
         comp = compare.Compare()
         result = comp.directories(self.current, self.base)
+        name = self.rev if self.rev != None else self.current
+
+        if result == True:
+            self._flag_as_good()
+        else:
+            self._flag_as_bad()
+
         return result
 
+    # legibility matters
     def _flag_as_good(self):
+        self.flag(self.rev, True)
+
+    def _flag_as_bad(self):
+        self.flag(self.rev, False)
+
+    def flag(self, revision, status=False):
+        status = 1 if status == True else 0
         if self.storage.is_valid():
-            self.storage.create_log(name=self.rev, status=1)
+            self.storage.create_log(name=revision, status=status)
 
 
     def check(self, revision=None):
-        return True
+        if self.storage.is_valid() == False or revision == None:
+            return False
+
+        all = self.storage.get(revision)
+        print all
+        return all[2] == 1
 
 
     def remove_db(self):
