04c7d3c7e52410c3bbd18da6cc5da03a02c4aab8
Felipe Knorr Kuhn <git@knorrium.info>
2012-10-05 19:59:47 -0300

    Severino now takes screenshots under screenshots/pagename/browser-pagename.png

--- test/pyccuracy/actions/screenshot.py
+++ test/pyccuracy/actions/screenshot.py
@@ -1,5 +1,6 @@
 from pyccuracy.actions import ActionBase
 from pyccuracy.errors import ActionFailedError
+import os
 
 class ScreenshotAction(ActionBase):
 	regex = r'(And )?I take a screenshot'
@@ -9,4 +10,16 @@ class ScreenshotAction(ActionBase):
 		page = context.current_page.__class__.__name__
 		filename = browser + "-" + page + ".png"
 		
-		raise ActionFailedError(filename)
+		#TODO: save to the base severino folder
+		path =  "screenshots/" + page
+		
+		if not os.path.exists(path):
+			os.makedirs(path)
+		
+		#TODO: increment file names
+		finalpath = path + "/" + filename
+
+		if hasattr(context.browser_driver, 'selenium'):
+			context.browser_driver.selenium.save_screenshot(finalpath)
+		elif hasattr(context.browser_driver, 'webdriver'):
+			context.browser_driver.webdriver.get_screenshot_as_file(finalpath)
