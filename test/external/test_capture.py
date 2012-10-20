f0ab6ba09cb25b6751fcee2b3937a71daf4810c8
Matheus Mendes <bigodines@gmail.com>
2012-10-20 16:28:30 -0300

    merge complete.

--- test/external/test_capture.py
+++ test/external/test_capture.py
@@ -1,6 +1,9 @@
 # Temporary file just to illustrate how to capture
 
-from ...severino.capture import capture
+import sys, os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'severino'))
+from severino import severino
+from ...app.capture import capture
 
 capturer = capture.SeverinoCapture()
 capturer.visit_and_capture()
